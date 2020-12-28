eee5dc7a35cff6dad042339fb6e567720159e9c7
aritchat <aritrachatterjee2007@gmail.com>
2020-12-28 17:44:33 +0530

    Adding new files

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //We can use external style files or also define localized styling for each component
+  //Method 1
+  //styleUrls: ['./app.component.css']
+  //Method 2
+  styles: [`
+  h3{
+    color: violet
+  }
+  `]
 })
 export class AppComponent {
   
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,8 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  //Element selector
+  //selector: 'app-servers',
+  //Attribute selector
+  //selector: '[app-servers]',
+  //Class selector
+  selector: '.app-servers',
+  //Method 1 of writing template
+  //templateUrl: './servers.component.html', //We can either specify a template html externally or define one locally
+  //Method 2 of writing template
+  //template: `<app-server></app-server>
+  //           <app-server></app-server>`,
+  //Method 3 of writing template
+  template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
