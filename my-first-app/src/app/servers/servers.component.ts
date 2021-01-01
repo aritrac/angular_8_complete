807ab80535f7b28be33286c63d68140fb6017f01
aritchat <aritrachatterjee2007@gmail.com>
2021-01-01 19:58:03 +0530

    Adding new code

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -8,19 +8,35 @@ import { Component, OnInit } from '@angular/core';
   //Class selector
   selector: '.app-servers',
   //Method 1 of writing template
-  //templateUrl: './servers.component.html', //We can either specify a template html externally or define one locally
+  templateUrl: './servers.component.html', //We can either specify a template html externally or define one locally
   //Method 2 of writing template
   //template: `<app-server></app-server>
   //           <app-server></app-server>`,
   //Method 3 of writing template
-  template: '<app-server></app-server><app-server></app-server>',
+  //template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!";
+  serverName = "TestServer";
+
+  constructor() { 
+    setTimeout(() =>{
+      this.allowNewServer = true;
+    },2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
