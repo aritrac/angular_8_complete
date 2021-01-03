46cffff859b1b6069c192a4ba2bad488ff4bbd11
aritchat <aritrachatterjee2007@gmail.com>
2021-01-03 18:43:59 +0530

    Completed property and event binding and view encapsulation

--- components-databinding-example-app/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ components-databinding-example-app/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,18 +9,20 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   //newServerName = ''; This is not being used as we are using local reference to fetch the server name value into typescript code
-  newServerContent = '';
+  //newServerContent = ''; This is not being used as we are using @ViewChild to fetch the server content value into typescript code
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  //Here serverName is being passes as method argument of a local reference which actually points to the element
   onAddServer(nameInput: HTMLInputElement) { //This is being passed the local reference of the input element that we defined in the template
-    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+    //Here serverContent is being accessed via ViewChild decorator which provides an elementRef which provides access to the underlying nativeElement to get its value
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) { //This is being passed the local reference of the input element that we defined in the template
-    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
   }
 }
--- components-databinding-example-app/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ components-databinding-example-app/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,16 +1,79 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, 
+         Input, 
+         OnInit, 
+         ViewEncapsulation,
+         OnChanges, 
+         SimpleChanges,
+         DoCheck,
+         AfterContentInit,
+         AfterContentChecked,
+         AfterViewInit,
+         AfterViewChecked,
+         OnDestroy,
+         ViewChild,
+         ElementRef,
+         ContentChild} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated //None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+       OnInit,  
+       OnChanges,  
+       DoCheck,  
+       AfterContentInit,
+       AfterContentChecked,
+       AfterViewInit,
+       AfterViewChecked,
+       OnDestroy {
+  //ngOnChanges() method will be called if any of the @Input params changes in the parent
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+  @ViewChild('heading',{static:true}) header: ElementRef;
+  @ContentChild('contentParagraph',{static: true}) paragraph: ElementRef;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called!');
+  }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngOnInit(){
+    console.log('ngOnInit called!');
+    console.log("View Child Text: " + this.header.nativeElement.textContent);
+    console.log("Content Child Text: " + this.paragraph.nativeElement.textContent);
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called!');
+    console.log("Content Child Text: " + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called!');
+    console.log("View Child Text: " + this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy called');
   }
 
 }
