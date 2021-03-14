ffcd9a8128a883e4e65a7a89ac63f276560c5efc
aritchat <aritrachatterjee2007@gmail.com>
2021-03-14 21:14:24 +0530

    221. Loading the Shopping List Items into the Form

--- course-app-basics/src/app/shopping-list/shopping-list.service.ts
+++ course-app-basics/src/app/shopping-list/shopping-list.service.ts
@@ -14,6 +14,10 @@ export class ShoppingListService {
         return this.ingredients.slice(); // returning a copy not the original array to the external world
     }
 
+    getIngredient(index: number){
+        return this.ingredients[index];
+    }
+
     addIngredient(ingredient: Ingredient){
         this.ingredients.push(ingredient);
         this.ingredientsChanged.next(this.ingredients.slice());
