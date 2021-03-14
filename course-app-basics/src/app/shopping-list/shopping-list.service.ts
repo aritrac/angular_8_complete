13a01fbab7ad688758e23d20692f32ead7788d53
aritchat <aritrachatterjee2007@gmail.com>
2021-03-14 22:13:53 +0530

    222. Updating existing items

--- course-app-basics/src/app/shopping-list/shopping-list.service.ts
+++ course-app-basics/src/app/shopping-list/shopping-list.service.ts
@@ -30,4 +30,9 @@ export class ShoppingListService {
         this.ingredients.push(...ingredients); //to append to the ingredients array as multiple elements instead of a single object
         this.ingredientsChanged.next(this.ingredients.slice());
     }
+
+    updateIngredient(index: number, newIngredient: Ingredient){
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
 }
