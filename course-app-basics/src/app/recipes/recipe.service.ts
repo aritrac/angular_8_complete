93918e58e4dd79648bc70d92e9ab2a64711ba438
aritchat <aritrachatterjee2007@gmail.com>
2021-04-19 11:56:33 +0530

    282. Fetching Recipes

--- course-app-basics/src/app/recipes/recipe.service.ts
+++ course-app-basics/src/app/recipes/recipe.service.ts
@@ -28,6 +28,11 @@ export class RecipeService {
 
     constructor(private shoppingListService: ShoppingListService){}
 
+    setRecipes(recipes: Recipe[]){
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
     getRecipes(){
         return this.recipes.slice(); //return copy of the array to outside, so it is not modified accidentally from outside
     }
--- course-app-basics/src/app/shared/data-storage.service.ts
+++ course-app-basics/src/app/shared/data-storage.service.ts
@@ -9,11 +9,19 @@ import { RecipeService } from "../recipes/recipe.service";
 export class DataStorageService {
     constructor(private http: HttpClient, private recipeService: RecipeService){}
 
-    storeRecipes(recipe: Recipe[]){
+    storeRecipes(){
         const recipes = this.recipeService.getRecipes();
         this.http.put('https://ng-course-recipe-book-2c7bc-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(
             response => {
                 console.log(response);
+            }
+        );
+    }
+
+    fetchRecipes(){
+        this.http.get<Recipe[]>('https://ng-course-recipe-book-2c7bc-default-rtdb.firebaseio.com/recipes.json',).subscribe(
+            recipes => {
+                this.recipeService.setRecipes(recipes);
             }
         );
     }
