3621a3ded72f55cd0038067a2df4fee5cdc0b102
aritchat <aritrachatterjee2007@gmail.com>
2021-04-19 11:32:05 +0530

    281. Storing recipes

--- course-app-basics/src/app/shared/data-storage.service.ts
+++ course-app-basics/src/app/shared/data-storage.service.ts
@@ -1,9 +1,20 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class DataStorageService {
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient, private recipeService: RecipeService){}
+
+    storeRecipes(recipe: Recipe[]){
+        const recipes = this.recipeService.getRecipes();
+        this.http.put('https://ng-course-recipe-book-2c7bc-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(
+            response => {
+                console.log(response);
+            }
+        );
+    }
 }
