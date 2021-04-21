7a31c19b566661c00e58eb4fd835b0b9c97d645d
aritchat <aritrachatterjee2007@gmail.com>
2021-04-21 10:03:53 +0530

    297. Sending Login Requests

--- auth-01-auth-component/src/app/auth/auth.service.ts
+++ auth-01-auth-component/src/app/auth/auth.service.ts
@@ -3,34 +3,44 @@ import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
 
-    constructor(private http: HttpClient){}
-    signup(email: string, password: string){
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCP-nrx1gXAEVGtK3kcl2QxpaXDAHkRnMc',{
-            email:email,
+    constructor(private http: HttpClient) { }
+
+    signup(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCP-nrx1gXAEVGtK3kcl2QxpaXDAHkRnMc', {
+            email: email,
             password: password,
             returnSecureToken: true
         }).pipe(catchError(errorRes => {
             let errorMesage = "An unknown error occurred!";
-            if(!errorRes.error || !errorRes.error.error){
+            if (!errorRes.error || !errorRes.error.error) {
                 return throwError(errorMesage);
             }
-            switch(errorRes.error.error.message){
+            switch (errorRes.error.error.message) {
                 case 'EMAIL_EXISTS':
-                  errorMesage = 'This email exists already';
-              }
-              return throwError(errorMesage);
+                    errorMesage = 'This email exists already';
+            }
+            return throwError(errorMesage);
         }));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCP-nrx1gXAEVGtK3kcl2QxpaXDAHkRnMc', {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        });
     }
 }
