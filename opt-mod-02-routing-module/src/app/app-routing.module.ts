b813059326efdf5d48c15265fa38c57a8d46dc43
aritchat <aritrachatterjee2007@gmail.com>
2021-05-02 09:39:14 +0530

    332. More Lazy Loading

--- opt-mod-02-routing-module/src/app/app-routing.module.ts
+++ opt-mod-02-routing-module/src/app/app-routing.module.ts
@@ -6,7 +6,15 @@ import { AuthComponent } from './auth/auth.component';
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes' ,
-    loadChildren: './recipes/recipes.module#RecipesModule'
+    loadChildren: './recipes/recipes.module#RecipesModule' //This is for lazy loading the module files, thereby reducing the main.js file size
+  },
+  {
+    path: 'shopping-list', 
+    loadChildren: './shopping-list/shopping-list.module#ShoppingListModule'
+  },
+  {
+    path: 'auth',
+    loadChildren: './auth/auth.module#AuthModule'
   }
 ];
 
