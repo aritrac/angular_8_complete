1f9f0df7b3753589e3e4ceacd7aa88a0874e1a08
aritchat <aritrachatterjee2007@gmail.com>
2021-05-13 10:36:21 +0530

    363. Auth Finished (For Now...)

--- auth-01-auth-component/src/app/auth/auth-interceptor.service.ts
+++ auth-01-auth-component/src/app/auth/auth-interceptor.service.ts
@@ -1,12 +1,14 @@
 import { HttpHandler, HttpInterceptor, HttpParams, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { exhaustMap, take } from "rxjs/operators";
+import { Store } from "@ngrx/store";
+
 import { AuthService } from "./auth.service";
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor{
 
-    constructor(private authService: AuthService){}
+    constructor(private authService: AuthService, private store: Store ){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler){
         return this.authService.user.pipe(take(1), exhaustMap(user => {
