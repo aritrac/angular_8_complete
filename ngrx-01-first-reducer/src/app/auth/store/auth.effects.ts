74ac35b83f24e658399e207b5c2b58755b4ba864
aritchat <aritrachatterjee2007@gmail.com>
2021-05-13 11:32:26 +0530

    367. Effects & Error Handling

--- ngrx-01-first-reducer/src/app/auth/store/auth.effects.ts
+++ ngrx-01-first-reducer/src/app/auth/store/auth.effects.ts
@@ -1,10 +1,43 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+
+import { environment } from '../../../environments/environment'
 
 import * as AuthActions from './auth.actions';
+
+export interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+    registered?: boolean;
+  }
+
 export class AuthEffects {
+    @Effect()
     authLogin = this.actions$.pipe(
-        ofType(AuthActions.LOGIN_START)
+        ofType(AuthActions.LOGIN_START),
+        switchMap((authData: AuthActions.LoginStart) => {
+            return this.http
+            .post<AuthResponseData>(
+                'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + environment.firebaseAPIKey,
+                {
+                email: authData.payload.email,
+                password: authData.payload.password,
+                returnSecureToken: true
+                }
+            ).pipe(catchError(error => {
+                of();//Creates new observable
+            }), map(resData => {
+                of();
+            }));
+        }),
+
     );
 
-    constructor( private actions$ : Actions) {}
+    constructor( private actions$ : Actions, private http: HttpClient) {}
 }
