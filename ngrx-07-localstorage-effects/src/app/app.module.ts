b9cc8daf90e2132fcefdba8701de447d27340afa
aritchat <aritrachatterjee2007@gmail.com>
2021-05-15 15:34:09 +0530

    376. Using Store Devtools Chrome Plugin

--- ngrx-07-localstorage-effects/src/app/app.module.ts
+++ ngrx-07-localstorage-effects/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule} from '@ngrx/store-devtools'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -20,6 +21,7 @@ import { AuthEffects } from './auth/store/auth.effects';
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({}),
     SharedModule,
     CoreModule
   ],
