17bbe152fdd465f02478b5e8ffd772497920d29e
aritchat <aritrachatterjee2007@gmail.com>
2021-05-15 19:11:32 +0530

    383. Storing Recipes via Effects

--- ngrx-07-localstorage-effects/src/app/recipes/store/recipe.effects.ts
+++ ngrx-07-localstorage-effects/src/app/recipes/store/recipe.effects.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, withLatestFrom } from 'rxjs/operators';
 
 import * as RecipesActions from './recipe.actions';
+import * as fromApp from '../../store/app.reducer';
 import { Recipe } from '../recipe.model';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Injectable()
 export class RecipeEffects {
@@ -23,9 +25,22 @@ export class RecipeEffects {
             });
         }),
         map(recipes => {
-          return new RecipesActions.SetRecipes(recipes);
+            return new RecipesActions.SetRecipes(recipes);
         })
     );
 
-    constructor(private actions$: Actions, private http: HttpClient) { }
+    @Effect({dispatch: false})
+    storeRecipes = this.actions$.pipe(
+        ofType(RecipesActions.STORE_RECIPES),
+        withLatestFrom(this.store.select('recipes')),
+        switchMap(([actionData, recipesState]) => {
+            return this.http
+                .put(
+                    'https://ng-course-recipe-book-2c7bc-default-rtdb.firebaseio.com/recipes.json',
+                    recipesState.recipes
+                )
+        })
+    );
+
+    constructor(private actions$: Actions, private http: HttpClient, private store: Store<fromApp.AppState>) { }
 }
